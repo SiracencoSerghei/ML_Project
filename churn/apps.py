07a3d6770deff8f86d60c1854e6775b7980b7905
Siracenco Serghei <siracencoserghei@gmail.com>
2026-03-22 14:09:28 +0100

    stable versions

--- churn/apps.py
+++ churn/apps.py
@@ -1,6 +1,19 @@
+import logging
+import sklearn
+import xgboost
+import joblib
+import numpy as np
 from django.apps import AppConfig
+
+logger = logging.getLogger(__name__)
 
 
 class ChurnConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "churn"
+
+    def ready(self):
+        logger.info(f"sklearn: {sklearn.__version__}")
+        logger.info(f"xgboost: {xgboost.__version__}")
+        logger.info(f"joblib: {joblib.__version__}")
+        logger.info(f"numpy: {np.__version__}")
--- churn_project/settings.py
+++ churn_project/settings.py
@@ -138,3 +138,16 @@ CACHES = {
         "LOCATION": "unique-ml-cache",
     }
 }
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
